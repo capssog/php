c05f325c968844ad66aa5ac47ecd0a478e4c17c8
Carlos Parada <capssog@gmail.com>
2023-02-13 23:11:40 -0500

    13022023_1

---
+++ if-else.php
@@ -0,0 +1,17 @@
+<?php
+
+$asientos_disponibles = 0;
+$es_hijo_CarlosP = true;
+
+if ($asientos_disponibles > 0 ){
+echo "Ok puedes trabajar";
+}
+elseif($es_hijo_CarlosP==true){
+    echo "no creo, pero todo ok";
+}
+else{
+echo "Se complico";
+}
+
+echo "\n";
+?>
---
+++ while.php
@@ -0,0 +1,10 @@
+<?php
+
+$contador = 0;
+
+while($contador <= 10) {
+    echo "Actualmente estamos en la iteración $contador \n";
+    $contador++;
+}
+
+echo "\n";
